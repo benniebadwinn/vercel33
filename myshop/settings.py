cf7cf5c0b01e15711d4ee60dad11b246448b3a88
benniebadwinn <benniebadwin@hotmail.com>
2023-02-10 00:01:28 +0300

    first commit

--- myshop/settings.py
+++ myshop/settings.py
@@ -124,6 +124,14 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static"
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
