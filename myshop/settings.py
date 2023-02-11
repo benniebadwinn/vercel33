6df5cf2990c70c0403d0ba8eddbbc2b41cd13e3f
benniebadwinn <benniebadwin@hotmail.com>
2023-02-11 21:54:29 +0300

    templates

--- myshop/settings.py
+++ myshop/settings.py
@@ -138,8 +138,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# # STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static"
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / ".vercel" / "static"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join( os.path.join(BASE_DIR), "media")
